ee735da25beb4ced858e3152cf3323fa8e1cdf0f
himanshudhimanux <himanshudhiman.hd34@gmail.com>
2023-03-10 16:20:30 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <>
+    {/* Sidebar Menu  */}
+      <Sidebar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Button} from 'react-bootstrap';
+
+const Sidebar = () => {
+  return (
+    <>
+
+      <Button className="mx-3 my-3" variant='success'>Hello React</Button>
+
+    </>
+  )
+}
+
+export default Sidebar
